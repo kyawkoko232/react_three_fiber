dbcc33826c776103bf5b9654b5fb405c30d80fb1
kyawkoko232 <kyawkoko9m@gmail.com>
2024-03-05 19:52:50 +0630

    test no size fixed

---
+++ src/Experience.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Experience = () => {
+    return <>
+
+    
+    <mesh>
+        <torusKnotGeometry />
+        <meshNormalMaterial />
+    </mesh>
+
+</>
+}
+
+export default Experience
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Canvas } from '@react-three/fiber'
+import Experience from './Experience.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Canvas 
+    camera={ { 
+      fov : 45,
+      near : 0.1,
+      position: [10,3,25]
+    } }
+    > 
+        <Experience />
+    </Canvas>
+
   </React.StrictMode>,
 )
